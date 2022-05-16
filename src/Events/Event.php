d26611b6fdce805cb2d71c3171b746de957a8aae
René Preuß <rene@preuss.io>
2022-05-16 11:33:58 +0200

    Remove client in serialization

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -3,16 +3,55 @@
 namespace GhostZero\Tmi\Events;
 
 use GhostZero\Tmi\Client;
+use ReflectionClass;
+use ReflectionException;
+use Serializable;
 
-class Event
+class Event implements Serializable
 {
     /**
-     * @var Client Client object
+     * @var Client|null Client object. Can be null if serialized.
      */
-    public Client $client;
+    public ?Client $client = null;
 
     public function signature(): ?string
     {
         return null;
+    }
+
+    /**
+     * Override serializer, since we cannot serialize the client object.
+     *
+     * @inheritdoc
+     */
+    public function serialize()
+    {
+        $data = [];
+        $reflect = new ReflectionClass($this);
+        $props = $reflect->getProperties();
+        foreach ($props as $prop) {
+            if ($prop->getName() === 'client') {
+                continue;
+            }
+            $data[$prop->getName()] = ['s' => $prop->isStatic(), 'v' => $prop->getValue($this)];
+        }
+        return serialize($data);
+    }
+
+    /**
+     * @inheritdoc
+     * @throws ReflectionException
+     */
+    public function unserialize($data)
+    {
+        $props = unserialize($data);
+        $reflect = new ReflectionClass($this);
+        foreach ($props as $name => $prop) {
+            if ($prop['s']) {
+                $reflect->getProperty($name)->setValue($prop['v']);
+            } else {
+                $reflect->getProperty($name)->setValue($this, $prop['v']);
+            }
+        }
     }
 }
